fccc0b51a1ba8c6a6255bb63f48a97bd7ac1041d
Michal Rybecky <michalrybec@protonmail.com>
2021-04-26 23:05:17 +0200

    Nove veci

---
+++ ulohy/17.3.py
@@ -0,0 +1,41 @@
+import tkinter
+
+c = tkinter.Canvas(height=500, width=1000)
+c.pack()
+
+text = input("Zadaj vyraz: ")
+prava = len([znak for znak in text if znak == ")"])
+lava = len([znak for znak in text if znak == "("])
+
+farby = ("red", "blue", "green", "yellow", "orange", "brown", "purple", "pink")
+
+
+for znak in text:
+    if znak == "(":
+        if znak == ")":
+            zle = True
+            break
+        elif znak == "(":
+            zle = False
+            break
+zle = True if prava != lava else False
+
+if not zle:
+    x = 500 - len(text) * 20 / 2
+    y, n = 100, 0
+    for znak in text:
+        if znak == "(":
+            farba = farby[n]
+            n += 1
+        elif znak == ")":
+            n -= 1
+            farba = farby[n]
+        else:
+            farba = "black"
+        c.create_text(x, y, text=znak, fill=farba, font="arial 40")
+        x += 20
+    c.create_text(500, 300, text="Zatvorkovanie je spravne!", font="arial 40")
+else:
+    c.create_text(500, 250, text="Zatvorkovanie nie je spravne!", font="arial 50")
+
+c.mainloop()
---
+++ ulohy/24.3.py
@@ -0,0 +1,16 @@
+with open('ziaci.txt', 'r') as file:
+    mena = [x.strip() for x in file.readlines()]
+
+slicer = int(len(mena) / 2)
+
+mena_1 = mena[:slicer] # krstne mena
+mena_2 = mena[slicer:] # priezviska
+
+print(f'Pocet mien v subore: {slicer}')
+print(f'Najdlhsie krstne meno: {max(mena_1, key=len)}')
+print(f'Najdlhsie priezvisko: {max(mena_2, key=len)}')
+
+with open('vystup.txt', 'w') as file:
+    for krstne, priezvisko in zip(mena_1, mena_2):
+        medzery = (20 - len(krstne))
+        file.write(str(krstne) + medzery * ' ' + str(priezvisko) + '\n')
